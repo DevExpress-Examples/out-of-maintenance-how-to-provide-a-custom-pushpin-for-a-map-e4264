493d1d77bd5382ca2522620d65970f92c09f8569
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/MapPushpinTemplate.Web/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("MapPushpinTemplate.Web")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MapPushpinTemplate.Web")]
+[assembly: AssemblyCopyright("Copyright ©  2012")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("80e4ef0b-722d-4013-968e-0e5227e50b17")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Readme.md
@@ -0,0 +1,10 @@
+# How to provide a custom pushpin for a map
+
+
+<p>This example demonstrates  how to implement a custom pushpin using its template. </p><br />
+<p>To accomplish this,  it is necessary to create a <strong>System.Windows.DataTemplate</strong> that contains the <strong>StackPanel</strong> with a custom pushpin image and<strong> TextBlock</strong> (specifies the pushpin title). </p><p>Then, add this data template to a window's resource and apply it to a map pushpin via the <a href="http://documentation.devexpress.com/#Silverlight/DevExpressXpfMapMapPushpin_Templatetopic"><u>MapPushpin.Template</u></a> property.</p><br />
+
+
+<br/>
+
+
